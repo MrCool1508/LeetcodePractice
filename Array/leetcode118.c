6f164f935667f0f25e7149c739c2876e2f2afcfb
MrCool1508 <hw531025@126.com>
2020-12-06 23:50:12 +0800

    12.06吧之前做错的杨辉三角补上了

--- Array/leetcode118.c
+++ Array/leetcode118.c
@@ -10,9 +10,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
 int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
-    int** res          = (int**)malloc(numRows * sizeof(int*));
-    *returnColumnSizes = (int*)malloc(sizeof(int));
+    int** res          = (int**)calloc(numRows, sizeof(int*));
+    *returnColumnSizes = (int*)calloc(numRows, sizeof(int));
     *returnSize        = numRows;
     //**returnColumnSizes = (int**)calloc(numRows, sizeof(int*));
 
@@ -21,19 +26,21 @@ int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
     }
 
     for (int i = 0; i < numRows; i++) {
-        res[i]    = (int*)malloc((i + 1) * sizeof(int));
-        res[i][0] = 1;
-        res[i][i] = 1;
+        (*returnColumnSizes)[i] = i + 1;
+        res[i]                  = (int*)calloc((i + 1), sizeof(int));
+        res[i][0]               = 1;
+        res[i][i]               = 1;
+        // printf("%3d ", res[i][0]);
         for (int j = 1; j < i; j++) {
             res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
-            printf("eR:%d ", res[i][j]);
+            // printf("%3d ", res[i][j]);
         }
-        printf("\r\n");
-    }
-    for (int i = 0; i < numRows; i++) {
-        returnColumnSizes[i][0] = i + 1;
-    }
+        if (i != 0) {
+            // printf("%3d ", res[i][i]);
+        }
 
+        // printf("\r\n");
+    }
     return res;
 }
 
